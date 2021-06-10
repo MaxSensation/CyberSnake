f3ae5515a53374ddeafac6da329903d779018f37
Maximiliam Rosén <karlsson.maximiliam@gmail.com>
2021-06-10 14:52:47 +0200

    Removed Wincondiotion

--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -1,7 +1,6 @@
 using System;
 using MLAPI;
 using MLAPI.Messaging;
-using MLAPI.NetworkVariable;
 using TMPro;
 using UnityEngine;
 
@@ -11,55 +10,14 @@ public class InGameMenu : NetworkBehaviour
     [SerializeField] private GameObject title;
     [SerializeField] private GameObject exit;
     [SerializeField] private GameObject restart;
-    private NetworkVariableInt _alivePlayers = new NetworkVariableInt(new NetworkVariableSettings{WritePermission = NetworkVariablePermission.ServerOnly});
-    private bool _alive = true;
     private void Start()
     {
-        PlaneController.onPlayerKilled += DecreasePlayers;
         PlaneController.onLocalPlayerStart += DisableMenu;
         PlaneController.onLocalPlayerKilled += EnableMenu;
-        PlaneController.onPlayerWon += EnableWinningMenu;
-        if (IsServer)
-        {
-            _alivePlayers.Value = NetworkManager.ConnectedClients.Count;
-        }
-    }
-
-    private void DecreasePlayers()
-    {
-        if (IsServer)
-        {
-            _alivePlayers.Value -= 1;
-        }
-    }
-
-    private void Update()
-    {
-        CheckForWin();
-    }
-
-    private void CheckForWin()
-    {
-        if (_alivePlayers.Value == 1 && _alive)
-        {
-            EnableWinningMenu();
-        }
-    }
-
-    private void EnableWinningMenu()
-    {
-        title.GetComponent<TMP_Text>().SetText("Winner!");
-        title.SetActive(true);
-        exit.SetActive(true);
-        if (IsServer)
-        {
-            restart.SetActive(true);
-        }
     }
 
     private void EnableMenu()
     {
-        _alive = false;
         title.SetActive(true);
         exit.SetActive(true);
         if (IsServer)
@@ -85,11 +43,6 @@ public class InGameMenu : NetworkBehaviour
 
     public void RestartGame()
     {
-        if (IsServer)
-        {
-            _alivePlayers.Value = NetworkManager.ConnectedClients.Count;
-        }
-        _alive = true;
         title.GetComponent<TMP_Text>().SetText("GameOver!");
         DisableMenu();
         onRestartEvent?.Invoke();
